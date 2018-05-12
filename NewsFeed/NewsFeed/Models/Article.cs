6c611b77b2090a106c57c17519a6938160a091e1
Peter Vitez <ptr.vtz@gmail.com>
2018-05-12 20:45:26 +0200

    Formatted view

--- NewsFeed/NewsFeed/Models/Article.cs
+++ NewsFeed/NewsFeed/Models/Article.cs
@@ -7,10 +7,22 @@ namespace NewsFeed.Models
     public class Article
     {
         public string Author { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        
+		public string Title { get; set; }
+        
+		public string Description { get; set; }
+        
         public string Url { get; set; }
-        public string UrlToImage { get; set; }
-        public string PublishedAt { get; set; }
+        
+		public string UrlToImage { get; set; }
+        
+		public string PublishedAt { private get; set; }
+
+		public DateTime GetDateTime
+		{
+			get => Convert.ToDateTime(PublishedAt); 
+			set => GetDateTime = value; 
+		}
+
     }
 }
